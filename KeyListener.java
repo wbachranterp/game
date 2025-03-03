b6acc3eff2b07a05e13b129000d8c7e05508371d
wbachranterp <wbachran@terpmail.umd.edu>
2025-03-03 15:37:40 -0500

    Add files via upload

---
+++ KeyListener.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.awt.event.*;
+
+public interface KeyListener {
+
+	public void keyPressed(KeyEvent e);
+
+	public void keyReleased(KeyEvent e);
+
+	public void keyTyped(KeyEvent e);
+
+}
---
+++ YourGameName.java
@@ -0,0 +1,56 @@
+package game;
+
+/*
+CLASS: YourGameNameoids
+DESCRIPTION: Extending Game, YourGameName is all in the paint method.
+NOTE: This class is the metaphorical "main method" of your program,
+      it is your control center.
+
+*/
+import java.awt.*;
+import java.awt.event.*;
+
+class YourGameName extends Game implements KeyListener {
+	static int counter = 0;
+
+  public YourGameName() {
+    super("YourGameName!",800,600);
+    this.setFocusable(true);
+	this.requestFocus();
+  }
+  
+	public void paint(Graphics brush) {
+    	brush.setColor(Color.black);
+    	brush.fillRect(0,0,width,height);
+    	
+    	// sample code for printing message for debugging
+    	// counter is incremented and this message printed
+    	// each time the canvas is repainted
+    	counter++;
+    	brush.setColor(Color.white);
+    	brush.drawString("Counter is " + counter,10,10);
+  }
+  
+	public static void main (String[] args) {
+   		YourGameName a = new YourGameName();
+		a.repaint();
+  }
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
