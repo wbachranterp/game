ca8c3facc6807ea79c79c58f5297e72c03082fdf
wbachranterp <wbachran@terpmail.umd.edu>
2025-03-06 23:53:43 -0500

    Update YourGameName.java

--- YourGameName.java
+++ YourGameName.java
@@ -12,45 +12,54 @@ import java.awt.event.*;
 
 class YourGameName extends Game implements KeyListener {
 	static int counter = 0;
+	private Ship ship;
 
-  public YourGameName() {
-    super("YourGameName!",800,600);
-    this.setFocusable(true);
-	this.requestFocus();
-  }
-  
+	public YourGameName() {
+		super("Asteroid", 800, 600);
+		this.setFocusable(true);
+		this.requestFocus();
+		Point[] shipShape = { new Point(0, 0), new Point(0, 10), new Point(5, 15), new Point(10, 10),
+				new Point(10, 0) };
+		
+		ship = new Ship(shipShape, new Point(300,400), 0);
+
+	}
+
 	public void paint(Graphics brush) {
-    	brush.setColor(Color.black);
-    	brush.fillRect(0,0,width,height);
-    	
-    	// sample code for printing message for debugging
-    	// counter is incremented and this message printed
-    	// each time the canvas is repainted
-    	counter++;
-    	brush.setColor(Color.white);
-    	brush.drawString("Counter is " + counter,10,10);
-  }
-  
-	public static void main (String[] args) {
-   		YourGameName a = new YourGameName();
+		brush.setColor(Color.black);
+		brush.fillRect(0, 0, width, height);
+
+		// sample code for printing message for debugging
+		// counter is incremented and this message printed
+		// each time the canvas is repainted
+		counter++;
+		brush.setColor(Color.white);
+		brush.drawString("Counter is " + counter, 10, 10);
+		
+		ship.Paint(brush);
+
+	}
+
+	public static void main(String[] args) {
+		YourGameName a = new YourGameName();
 		a.repaint();
-  }
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
