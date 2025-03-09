174147b593c793e0a8f2b2b9acf5b1a89b44d68e
wbachranterp <wbachran@terpmail.umd.edu>
2025-03-09 14:45:47 -0400

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -2,9 +2,17 @@ package game;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Ship extends Polygon {
+public class Ship extends Polygon implements KeyListener {
 	 private Point[] shipPoints;
+	 private int stepSize = 3;
+	 private boolean forward;
+	 private boolean right;
+	 private boolean left;
+	 private boolean down;
+	 private int rotate;
 	
 	public Ship(Point[] inShape, Point inPosition, double inRotation) {
 		super(inShape, inPosition, inRotation);
@@ -24,13 +32,81 @@ public class Ship extends Polygon {
 			xArray[i] = (int)getPoints[i].getX();
 			yArray[i] = (int)getPoints[i].getY();
 		}
-		brush.setColor(Color.white);
+		brush.setColor(Color.RED);
 		brush.drawPolygon(xArray, yArray, getPoints.length);
+		brush.fillPolygon(xArray, yArray, getPoints.length);
 	}
 	
-	
+	public void move() {
+		if(forward) { 
+			position.setX(position.getX() - stepSize * Math.cos(Math.toRadians(rotation)));
+			position.setY(position.getY() - stepSize * Math.sin(Math.toRadians(rotation)));
+		}
+		if(left) { 
+			rotate(-2);
+			
+		}
+		if(right) { 
+			rotate(2);
+		}
+//		if(down) { 
+//			position.setX(position.getX());
+//			position.setY(position.getY() + stepSize);
+//		}
+		
+	}
 	
 	public Point[] getPArray() {
 		return shipPoints;
 	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		
+		if(keyCode == KeyEvent.VK_UP) {
+			forward = true;
+		}
+		if(keyCode == KeyEvent.VK_LEFT) {
+			left = true;
+			
+		}
+		if(keyCode == KeyEvent.VK_RIGHT) {
+			right = true;
+		}	
+		if(keyCode == KeyEvent.VK_DOWN) {
+			down = true;
+		}
+		
+		
+	}
+
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if(keyCode == KeyEvent.VK_UP) {
+			forward = false;
+		}
+		if(keyCode == KeyEvent.VK_LEFT) {
+			left = false;
+		}
+		if(keyCode == KeyEvent.VK_RIGHT) {
+			right = false;
+		}
+		if(keyCode == KeyEvent.VK_DOWN) {
+			down = false;
+		}
+		
+	}
+
+	
+
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
